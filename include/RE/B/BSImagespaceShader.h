e53bcb18f32e23a58fb2abc956f7069894970c57
Ilya Perapechka <i_perapechka@wargaming.net>
2023-03-16 23:04:26 +0200

    Imagespace shader improvements.

--- include/RE/B/BSImagespaceShader.h
+++ include/RE/B/BSImagespaceShader.h
@@ -5,6 +5,8 @@
 
 namespace RE
 {
+	class BSComputeShader;
+
 	class BSImagespaceShader :
 		public BSShader,         // 000
 		public ImageSpaceEffect  // 090
@@ -26,20 +28,19 @@ namespace RE
 		virtual void Unk_0B(void);  // 0B - { return; }
 		virtual void Unk_0C(void);  // 0C
 		virtual void Unk_0D(void);  // 0D
-		virtual void Unk_0E(void);  // 0E
+		virtual void LoadShaders();  // 0E
 
 		// members
-		BSFixedString			name;					// 120
-		BSFixedString			unk128;					// 128
-		std::uint64_t			unk130;					// 130
-		BSTArray<BSFixedString> vsConstantNames;		// 138
-		BSTArray<BSFixedString> psConstantNames;        // 150
-		BSTArray<BSFixedString> samplerNames;			// 158
-		std::uint64_t			unk180;					// 180
-		std::uint64_t			unk188;					// 188
-		std::uint64_t			unk190;					// 190
-		std::uint64_t			unk198;					// 198
-		std::uint64_t			unk1A0;					// 1A0
+		BSFixedString           name;                // 120
+		BSFixedString           originalShaderName;  // 128
+		std::uint64_t           unk130;              // 130
+		BSTArray<BSFixedString> vsConstantNames;     // 138
+		BSTArray<BSFixedString> psConstantNames;     // 150
+		BSTArray<BSFixedString> samplerNames;        // 158
+		BSTArray<BSFixedString> unk180;              // 180
+		BSComputeShader*        computeShader;       // 198
+		bool                    unk1A0;              // 1A0
+		bool                    isComputeShader;     // 1A1
 	};
 	static_assert(sizeof(BSImagespaceShader) == 0x1A8);
 }
